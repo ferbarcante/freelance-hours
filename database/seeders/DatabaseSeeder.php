ddf53bac566e3e582e5f62b584442cda3b41813b
ferbarcante <barcantef@gmail.com>
2024-10-09 10:16:03 -0300

    tabela projects

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\Project;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -15,5 +16,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory()->count(5)->create();
+
+        Project::factory()->count(5)->create();
     }
 }
